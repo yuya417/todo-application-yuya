c2058cc47d24e8255081cf80fdca3b77b6222e9b
鎌田裕也 <ykprogramdiv417@gmail.com>
2020-11-08 16:01:16 +0900

    コメントが投稿できない

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,4 +6,21 @@ class CommentsController < ApplicationController
     @comment = task.comments.build
   end
 
+  def create
+    task = Task.find(params[:task_id])
+    @comment = task.comments.build(comment_params)
+    binding.pry
+    if @comment.save
+      redirect_to board_task_path(task), notice: 'コメントを追加しました！'
+    else
+      flash.now[:error] = 'コメントを投稿できませんでした'
+      render :new
+    end
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:content).merge(user_id: current_user.id)
+  end
+
 end
